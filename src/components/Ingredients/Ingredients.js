07c9d6def14327643822bec69b1b2d5becf5f7a6
matcha <massahiro.endo@gmail.com>
2022-04-26 00:08:02 +0200

    useEffect hooks with async/await used inside the hook not directly. & a second useEffect to see the rendering times.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,11 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
 
 const Ingredients = () => {
-  const [userIgredients, setUserIngredients] = useState([]);
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://react-http-13cfc-default-rtdb.europe-west1.firebasedatabase.app//ingredients.json',
+      );
+      const responseData = await response.json();
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount,
+        });
+      }
+      setUserIngredients(loadedIngredients);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log('Rendering Ingredients', userIngredients);
+  }, [userIngredients]);
 
   const addIngredientHandler = async (ingredient) => {
     const response = await fetch(
@@ -31,7 +54,7 @@ const Ingredients = () => {
 
       <section>
         <Search />
-        <IngredientList ingredients={userIgredients} onRemoveItem={removeIngredientHandler} />
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
