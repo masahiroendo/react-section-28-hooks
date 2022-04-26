d8f3660271d4e1b7a5fa674f38ede96d487d19ca
matcha <massahiro.endo@gmail.com>
2022-04-27 00:24:36 +0200

    set useEffect hook in <Search /> and props object destructure In <Ingredients /> we add useCallback to the function passed to the props onLoadIngredients toward <Search />

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,28 +7,32 @@ import Search from './Search';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        'https://react-http-13cfc-default-rtdb.europe-west1.firebasedatabase.app//ingredients.json',
-      );
-      const responseData = await response.json();
-      const loadedIngredients = [];
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount,
-        });
-      }
-      setUserIngredients(loadedIngredients);
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch(
+  //       'https://react-http-13cfc-default-rtdb.europe-west1.firebasedatabase.app//ingredients.json',
+  //     );
+  //     const responseData = await response.json();
+  //     const loadedIngredients = [];
+  //     for (const key in responseData) {
+  //       loadedIngredients.push({
+  //         id: key,
+  //         title: responseData[key].title,
+  //         amount: responseData[key].amount,
+  //       });
+  //     }
+  //     setUserIngredients(loadedIngredients);
+  //   };
+  //   fetchData();
+  // }, []);
 
   useEffect(() => {
     console.log('Rendering Ingredients', userIngredients);
   }, [userIngredients]);
+
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+    setUserIngredients(filteredIngredients);
+  }, []);
 
   const addIngredientHandler = async (ingredient) => {
     const response = await fetch(
@@ -59,7 +63,7 @@ const Ingredients = () => {
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
-const Search = React.memo(props => {
+const Search = React.memo((props) => {
+  const { onLoadIngredients } = props;
+  const [searchName, setSearchName] = useState('');
+
+  useEffect(() => {
+    const query = searchName.length === 0 ? '' : `?orderBy="title"&equalTo="${searchName}"`;
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://react-http-13cfc-default-rtdb.europe-west1.firebasedatabase.app//ingredients.json' + query,
+      );
+      const responseData = await response.json();
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount,
+        });
+      }
+      onLoadIngredients(loadedIngredients);
+    };
+    fetchData();
+  }, [searchName, onLoadIngredients]);
+
+  const enteredNameHandler = (event) => {
+    setSearchName(event.target.value);
+  };
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
-          <label><b>Filter by Title</b></label>
-          <input type="text" />
+          <label>
+            <b>Filter by Title</b>
+          </label>
+          <input type="text" value={searchName} onChange={enteredNameHandler} />
         </div>
       </Card>
     </section>
