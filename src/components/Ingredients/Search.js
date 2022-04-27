db4259bf5dc4a685a66a7e41f20b2271ec30fdd7
matcha <massahiro.endo@gmail.com>
2022-04-27 11:49:58 +0200

    intro useRef hook to avoid multiple request with setTimeout( ) & clearTimout( ), inside useEffect

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,26 +6,34 @@ import './Search.css';
 const Search = React.memo((props) => {
   const { onLoadIngredients } = props;
   const [searchName, setSearchName] = useState('');
+  const nameInputRef = useRef();
 
   useEffect(() => {
-    const query = searchName.length === 0 ? '' : `?orderBy="title"&equalTo="${searchName}"`;
-    const fetchData = async () => {
-      const response = await fetch(
-        'https://react-http-13cfc-default-rtdb.europe-west1.firebasedatabase.app//ingredients.json' + query,
-      );
-      const responseData = await response.json();
-      const loadedIngredients = [];
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount,
-        });
+    const timer = setTimeout(() => {
+      if (searchName === nameInputRef.current.value) {
+        const query = searchName.length === 0 ? '' : `?orderBy="title"&equalTo="${searchName}"`;
+        const fetchData = async () => {
+          const response = await fetch(
+            'https://react-http-13cfc-default-rtdb.europe-west1.firebasedatabase.app//ingredients.json' + query,
+          );
+          const responseData = await response.json();
+          const loadedIngredients = [];
+          for (const key in responseData) {
+            loadedIngredients.push({
+              id: key,
+              title: responseData[key].title,
+              amount: responseData[key].amount,
+            });
+          }
+          onLoadIngredients(loadedIngredients);
+        };
+        fetchData();
       }
-      onLoadIngredients(loadedIngredients);
+    }, 500);
+    return () => {
+      clearTimeout(timer);
     };
-    fetchData();
-  }, [searchName, onLoadIngredients]);
+  }, [searchName, onLoadIngredients, nameInputRef]);
 
   const enteredNameHandler = (event) => {
     setSearchName(event.target.value);
@@ -38,7 +46,7 @@ const Search = React.memo((props) => {
           <label>
             <b>Filter by Title</b>
           </label>
-          <input type="text" value={searchName} onChange={enteredNameHandler} />
+          <input type="text" value={searchName} ref={nameInputRef} onChange={enteredNameHandler} />
         </div>
       </Card>
     </section>
