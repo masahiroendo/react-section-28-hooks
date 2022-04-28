d98762d48c12b61fc9485b17e9c710e19f5aefca
matcha <massahiro.endo@gmail.com>
2022-04-28 13:05:13 +0200

    feat: add loader to search component

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,36 +1,40 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
+import LoadingIndicator from '../UI/LoadingIndicator';
 import './Search.css';
+
+const fetchIngredients = async (searchName, callback) => {
+  const query = searchName.length === 0 ? '' : `?orderBy="title"&equalTo="${searchName}"`;
+  const response = await fetch(
+    'https://react-http-13cfc-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' + query,
+  );
+  const responseData = await response.json();
+  const loadedIngredients = [];
+  for (const key in responseData) {
+    loadedIngredients.push({
+      id: key,
+      title: responseData[key].title,
+      amount: responseData[key].amount,
+    });
+  }
+  callback(loadedIngredients);
+};
 
 const Search = React.memo((props) => {
   const { onLoadIngredients } = props;
   const [searchName, setSearchName] = useState('');
+  const [load, setLoad] = useState(false);
   const nameInputRef = useRef();
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    setLoad(true);
+    const timer = setTimeout(async () => {
       if (searchName !== nameInputRef.current.value) {
         return;
       }
-
-      const query = searchName.length === 0 ? '' : `?orderBy="title"&equalTo="${searchName}"`;
-      const fetchData = async () => {
-        const response = await fetch(
-          'https://react-http-13cfc-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' + query,
-        );
-        const responseData = await response.json();
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          });
-        }
-        onLoadIngredients(loadedIngredients);
-      };
-      fetchData();
+      await fetchIngredients(searchName, onLoadIngredients);
+      setLoad(false);
     }, 500);
 
     return () => {
@@ -49,6 +53,7 @@ const Search = React.memo((props) => {
           <label>
             <b>Filter by Title</b>
           </label>
+          {load && <LoadingIndicator />}
           <input type="text" value={searchName} ref={nameInputRef} onChange={enteredNameHandler} />
         </div>
       </Card>
